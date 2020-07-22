653508cfa5109951e2e29a8778459632cacf4a06
pargys <ps@sem-wolf.org>
2020-07-22 20:22:08 +0300

    support for input with one incorrect character

--- autocomplete.py
+++ autocomplete.py
@@ -1,52 +1,46 @@
-from data import data_dict
+from data import *
 from autocomplete_data import AutoCompleteData
 from auxiliary_functions import *
 
 
-def replace_character(prefix, best_completion):
-    equal = -1
-    results_indexes = []
+def replace_character(prefix, on_top):
+    results = []
     for i in range(len(prefix)-1, -1, -1):
-        if(-1 == equal):
-            for letter in range(ord('a'), ord('z')):
-                corrected_prefix = prefix[:i]+letter+prefix[i+1:]
-                indexes = data_dict.get(corrected_prefix)
-                if indexes:
-                    equal=i
-                    results_indexes = [index for index in indexes]
-                    break
-    corrected_prefix = prefix[:i] + letter + prefix[i + 1:]
-    if equal > 0:
-        completed_sentences = [data_dict[index] for index in results_indexes]
-        completed_sentences.sort()
+        if len(results) != 0:
+            break
+        for letter in range(ord('a'), ord('z')+1):
+            corrected_prefix = prefix[:i] + chr(letter) + prefix[i+1:]
+            indexes = data_dict.get(corrected_prefix)
+            if indexes:
+                score = 5 - i if i < 5 else 1
+                for j in indexes:
+                    results.append({"sentence": j, "source": get_source(j), "offset": get_offset(sentences_list[j], corrected_prefix), "score": len(prefix)*2 - score})
+                break
+    return results[:on_top]
 
-    score = 5 - equal if equal < 5 else 1
-    on_top = 5 - len(best_completion)
-    num_of_elements = on_top if len(results_indexes) > on_top else len(results_indexes)
-    for i in range(num_of_elements):
-        best_completion.append(AutoCompleteData(data_dict[results_indexes[i]], "", get_offset((results_indexes[i], corrected_prefix)), len(prefix) * 2 - score))
+
+def fix_word(prefix, on_top):
+    change_list = replace_character(prefix, on_top)
+    # insert_list = insert_char()
+    # delete_list = delete_char()
+    return change_list
 
 
 def get_best_k_completions(prefix):
     count_characters = len(prefix)
     prefix = ignore_delimiters(prefix)
     results_indexes = [index for index in data_dict[prefix]]
-    completed_sentences = [data_dict[index] for index in results_indexes]
+    completed_sentences = [sentences_list[index] for index in results_indexes]
 
-    if len(results_indexes) > 5:
-        completed_sentences.sort()
-
-    results_len = 5 if len(results_indexes) > 5 else len(results_indexes)
     best_completion = []
 
-    # -----I did not finish
-    for i in range(results_len):
-        best_completion.append(AutoCompleteData(data_dict[completed_sentences[i]], get_offset(completed_sentences[i], prefix), count_characters*2))
+    for i in range(len(results_indexes)):
+        best_completion.append(AutoCompleteData(completed_sentences[i], get_source(sentences_list.index(completed_sentences[i])), get_offset(completed_sentences[i],prefix), count_characters*2))
 
-    if len(results_indexes) < 5:
-        results_indexes.append(replace_character(prefix, best_completion))
+    results_dict = fix_word(prefix, 5 - len(results_indexes))
+    print(len(results_dict))
+    for result in results_dict:
+        best_completion.append(AutoCompleteData(sentences_list[result["sentence"]], result["source"], result["offset"], result["score"]))
 
-    if len(results_indexes) < 5:
-        # מחיקה של תוים
-        pass
+    return best_completion
 
