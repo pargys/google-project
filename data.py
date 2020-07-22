fb084924b73ac196cc83030804d9665b940872e1
pargys <ps@sem-wolf.org>
2020-07-22 14:11:09 +0300

    save just 5 sentences for all prefix

--- data.py
+++ data.py
@@ -1,16 +1,31 @@
 from collections import defaultdict
 from auxiliary_functions import *
 
-with open("file.txt") as file:
-    sentences_list = file.read().split("\n")
 
 data_dict = defaultdict(set)
-src_dict = {}
+sources_dict = {}
+sentences_list = []
 
-for i in range(len(sentences_list)):
-    for j in range(len(sentences_list[i])):
-        for k in range(j+1, len(sentences_list[i])+1):
-            data_dict[ignore_delimiters(sentences_list[i][k:j])].add(i)
-            data_dict[ignore_delimiters(sentences_list[i][j:k])].add(i)
+def init():
+    global sentences_list
+    with open("about.txt") as file:
+        sentences_list = file.read().split("\n")
+    for i in range(len(sentences_list)):
+        for j in range(len(sentences_list[i])):
+            for k in range(j+1, len(sentences_list[i])+1):
+                data_dict[ignore_delimiters(sentences_list[i][k:j])].add(i)
+                data_dict[ignore_delimiters(sentences_list[i][j:k])].add(i)
 
-data_dict.pop(" ")
+    data_dict.pop("")
+
+    for i in range(len(sentences_list)):
+        sources_dict[i] = "file.txt"
+
+    for prefix in data_dict.keys():
+        if len(data_dict[prefix]) > 5:
+            sentences = [sentences_list[index] for index in data_dict[prefix]]
+            sentences.sort()
+            data_dict[prefix] = set([sentences_list.index(sentence) for sentence in sentences][:5])
+
+init()
+print(data_dict)
