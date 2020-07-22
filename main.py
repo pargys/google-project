729db4e4fe591c7e80dd908a870463803ec1ccac
pargys <ps@sem-wolf.org>
2020-07-22 14:12:20 +0300

    add start_up function

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from autocomplete import *
+from data import sentences_list, init
+from autocomplete_data import AutoCompleteData
 
-print('please start search:')
+print(sentences_list)
 input_ = input()
-get_best_k_completions(input_)
+
+def start_app():
+
+    while True:
+        print('please start search:')
+
+        while input_[len(input_)-1] != '#':
+            print("Here are the results:")
+            best_completion = get_best_k_completions(input_)
+            for sentence in best_completion:
+                print(sentence.get_completed_sentence())
+            print(input_, end="")
+            input_ = input_ + input()
+
+start_app()
