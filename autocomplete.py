c8f6fd06ef64a013e0ed24618f62b0aa4d09e9da
pargys <ps@sem-wolf.org>
2020-07-22 22:19:51 +0300

    support for input with one miss or unnecessary  character

--- autocomplete.py
+++ autocomplete.py
@@ -19,11 +19,43 @@ def replace_character(prefix, on_top):
     return results[:on_top]
 
 
+def delete_char(prefix, on_top):
+    results = []
+    for i in range(len(prefix)-1, -1, -1):
+        if len(results) != 0:
+            break
+        for letter in range(ord('a'), ord('z')+1):
+            corrected_prefix = prefix[:i] + chr(letter) + prefix[i:]
+            indexes = data_dict.get(corrected_prefix)
+            if indexes:
+                score = 10 - 2*i if i < 4 else 2
+                for j in indexes:
+                    results.append({"sentence": j, "source": get_source(j), "offset": get_offset(sentences_list[j], corrected_prefix), "score": len(prefix)*2 - score})
+                break
+    return results[:on_top]
+
+
+def insert_char(prefix, on_top):
+    results = []
+    for i in range(len(prefix)-1, -1, -1):
+        corrected_prefix = prefix[:i] + prefix[i+1:]
+        indexes = data_dict.get(corrected_prefix)
+        if indexes:
+            score = 10 - 2*i if i < 4 else 2
+            for j in indexes:
+                results.append({"sentence": j, "source": get_source(j), "offset": get_offset(sentences_list[j], corrected_prefix), "score": len(prefix)*2 - score})
+            break
+    return results[:on_top]
+
+
 def fix_word(prefix, on_top):
     change_list = replace_character(prefix, on_top)
-    # insert_list = insert_char()
-    # delete_list = delete_char()
-    return change_list
+    insert_list = insert_char(prefix, on_top)
+    delete_list = delete_char(prefix, on_top)
+    correction_list = change_list + insert_list + delete_list
+    correction_list = sorted(correction_list, key=lambda k: k["score"], reverse=True)
+    correction_list = correction_list[:on_top]
+    return correction_list
 
 
 def get_best_k_completions(prefix):
@@ -38,7 +70,6 @@ def get_best_k_completions(prefix):
         best_completion.append(AutoCompleteData(completed_sentences[i], get_source(sentences_list.index(completed_sentences[i])), get_offset(completed_sentences[i],prefix), count_characters*2))
 
     results_dict = fix_word(prefix, 5 - len(results_indexes))
-    print(len(results_dict))
     for result in results_dict:
         best_completion.append(AutoCompleteData(sentences_list[result["sentence"]], result["source"], result["offset"], result["score"]))
 
