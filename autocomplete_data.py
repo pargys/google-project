9ff5f45a01712a46db61e58a6752b54c5a357a98
pargys <ps@sem-wolf.org>
2020-07-22 04:26:16 +0300

    AutoCompleteData class

---
+++ autocomplete_data.py
@@ -0,0 +1,20 @@
+
+class AutoCompleteData:
+    def __init__(self, completed_sentence, source_text, offset, score):
+        self.__completed_sentence = completed_sentence
+        self.__source_text = source_text
+        self.__offset = offset
+        self.__score = score
+
+    def get_completed_sentence(self):
+        return self.__completed_sentence
+
+    def get_source_text(self):
+        return self.__source_text
+
+    def get_offset(self):
+        return self.__offset
+
+    def get_score(self):
+        return self.__score
+
