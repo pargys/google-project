3f2d5eba17871a466d0438db0bcb66c55802fd9e
pargys <ps@sem-wolf.org>
2020-07-23 13:22:02 +0300

    support for input for which there are no results

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from autocomplete import *
-from data import sentences_list, init
-from autocomplete_data import AutoCompleteData
+from data import init
 
-print(sentences_list)
-input_ = input()
+init()
+
 
 def start_app():
-
     while True:
         print('please start search:')
+        input_ = input()
+        if input_ != "":
+            while input_[-1] != '#':
+                best_completion = get_best_k_completions(input_)
+                if best_completion:
+                    for sentence in best_completion:
+                        print(f"{sentence.get_completed_sentence()}:\n"
+                              f"source: {sentence.get_source_text()}, offset: {sentence.get_offset()}, score: {sentence.get_score()}\n")
+                    print(input_, end="")
 
-        while input_[len(input_)-1] != '#':
-            print("Here are the results:")
-            best_completion = get_best_k_completions(input_)
-            for sentence in best_completion:
-                print(sentence.get_completed_sentence())
-            print(input_, end="")
-            input_ = input_ + input()
+                else:
+                    print('no results found!')
+                    break
+
+                input_ = input_ + input()
+
 
 start_app()
